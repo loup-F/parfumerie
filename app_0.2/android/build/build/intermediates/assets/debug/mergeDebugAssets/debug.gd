80725eb99dc0c435406eea46d67e43f90441ec18
loup-F <72079712+loup-F@users.noreply.github.com>
2023-12-15 11:48:21 +0100

    ajout raycast et tout ! on va pouvoir faire des trucs

--- app_0.2/android/build/build/intermediates/assets/debug/mergeDebugAssets/debug.gd
+++ app_0.2/android/build/build/intermediates/assets/debug/mergeDebugAssets/debug.gd
@@ -1,11 +1,20 @@
 extends XROrigin3D
+@onready var ray_cast_3d = $XRCamera3D/RayCast3D
+@onready var label_3d_2 = $"../Label3D2"
+@onready var label_3d = $"../Label3D"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	label_3d_2.set_text("jaaj")
+	label_3d.set_text("jaaj")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(self.position)
+	if ray_cast_3d.get_collider():
+		label_3d_2.set_text("colliding")
+		label_3d.set_text("colliding")
+	else: 
+		label_3d.set_text("nope")
+		label_3d_2.set_text("nope")
